33cf536693b1b71923bf319a733b516c8970fb41
Vinícius Roque <viniciusfr6@gmail.com>
2023-03-17 17:17:23 -0300

    feat(): implemented unit test for not create a voucher if existscode

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,9 @@
-export default {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  moduleDirectories: ["node_modules", "src"],
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/*.spec.ts"],
+  moduleDirectories: ["node_modules", "src"],
   transform: {
     ".+\\.ts$": "ts-jest",
   },
+  testMatch: ["<rootDir>/tests/*/*.(test|spec).ts"],
 };
---
+++ tests/unit/voucherService.test.ts
@@ -0,0 +1,27 @@
+import { jest } from "@jest/globals";
+import voucherRepository from "repositories/voucherRepository";
+import voucherService from "services/voucherService";
+
+describe("create Voucher test suite", () => {
+  jest
+    .spyOn(voucherRepository, "createVoucher")
+    .mockImplementation((): any => { });
+
+  it("should not create a voucher if code already exists", async () => {
+    const voucher = {
+      code: "aaa",
+      discount: 10,
+    };
+
+    jest
+      .spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce({ ...voucher, used: false, id: 1 });
+
+    const promise = voucherService.createVoucher(voucher.code, voucher.discount);
+
+    expect(promise).rejects.toEqual({
+      message: "Voucher already exist.",
+      type: "conflict",
+    });
+  });
+});
